f8fb081cc38ba71df587e4b9b382e69200c04d74
Sean O <seanodotcom@yahoo.com>
2023-03-20 15:00:35 -0400

    Initial app skeleton with Tailwind CSS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="navbar">
+	<a href="/">Home</a>
+	<a href="/results">Results</a>
+</div>
+
+<div class="container">
+	<slot />
+</div>
+
+<style>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        background-color: #333;
+        color: #eee;
+        padding: 10px;
+		margin-bottom: 1em;
+    }
+    .navbar a {
+        color: #eee;
+        text-decoration: none;
+        padding: 4px;
+    }
+    .navbar a:hover {
+        color: #ccc;
+    }
+    .navbar a.active {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    :global(body) {
+        margin: 0;
+    }
+    .container {
+        max-width: 90%;
+        margin: 0 auto;
+        width: 1200px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-xl mb-8 font-bold">Welcome to Svelte Lottery!</h1>
+
+<main>
+    <p>
+        Keep on scratchin'!
+    </p>
+</main>
